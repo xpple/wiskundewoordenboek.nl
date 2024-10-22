0ace78da43ba63956cd81df70c59242ef51867f7
xpple <fred@xpple.dev>
2024-10-22 22:48:15 +0200

    Simplify LetterApiController

--- api/controllers/LetterApiController.php
+++ api/controllers/LetterApiController.php
@@ -9,21 +9,16 @@ use app\util\HttpException;
 class LetterApiController extends SuccessController {
     #[\Override]
     public function handle(array $path): void {
-        switch (count($path)) {
-            case 0:
-                throw HttpException::notFound();
-            case 1:
-                $letter = array_shift($path);
-                if (mb_strlen($letter) !== 1) {
-                    throw HttpException::notFound();
-                }
-                $databaseHelper = DatabaseHelper::getInstance();
-                $wordModels = $databaseHelper->getWordsForLetter($letter);
-                header('Content-Type: application/json');
-                echo json_encode($wordModels);
-                return;
-            default:
-                throw HttpException::notFound();
+        if (count($path) !== 1) {
+            throw HttpException::notFound();
         }
+        $letter = array_shift($path);
+        if (mb_strlen($letter) !== 1) {
+            throw HttpException::notFound();
+        }
+        $databaseHelper = DatabaseHelper::getInstance();
+        $wordModels = $databaseHelper->getWordsForLetter($letter);
+        header('Content-Type: application/json');
+        echo json_encode($wordModels);
     }
 }
